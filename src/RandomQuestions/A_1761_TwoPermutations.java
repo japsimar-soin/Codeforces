f7648e3cbcf3278b0d1d4376fb45abee92d0d59f
japsimar <japsimar@Ishu-3.local>
2024-03-30 18:13:44 +0530

    30Mar24

---
+++ src/RandomQuestions/A_1761_TwoPermutations.java
@@ -0,0 +1,31 @@
+package RandomQuestions;
+import java.util.*;
+
+public class A_1761_TwoPermutations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        boolean[] arr = new boolean[t];
+        
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            arr[i] = solve(n, a, b);
+        }
+        
+        for (int i = 0; i < t; i++) {
+            System.out.println(arr[i] ? "YES" : "NO");
+        }
+    }
+    
+    public static boolean solve(int n, int a, int b){
+        if(a == n && b == n){
+            return true;
+        }
+        boolean ans;
+        ans = n-(a+b) > 1;
+        return ans;
+    }
+}
---
+++ src/RandomQuestions/A_1788_OneTwo.java
@@ -0,0 +1,48 @@
+//package RandomQuestions;
+import java.util.*;
+
+public class A_1788_OneTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        int[] ans = new int[t];
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int j = 0; j < n; j++) {
+                arr[j] = sc.nextInt();
+            }
+            int val = solve(n, arr);
+            ans[i] = val;
+        }
+        for (int i = 0; i < t; i++) {
+            System.out.println(ans[i]);
+        }
+    }
+    
+    public static int solve(int n, int[] arr){
+        int ans = -1;
+        int count = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i] == 2){
+                count++;
+            }
+        }
+        if(count%2 == 1){
+            return -1;
+        }
+        int[] freq = new int[n];
+        int two = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i] == 2) {
+                two++;
+            }
+            freq[i] = two;
+            if(count-two == two){
+                return i+1;
+            }
+        }
+        return ans;
+    }
+}
