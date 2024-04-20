48e13571905a32c27846462bd8e1c76f287e37f5
japsimar <japsimar@Ishu-3.local>
2024-04-20 20:43:00 +0530

    20Apr24

---
+++ src/RandomQuestions/A_1155_ReverseASubstring.java
@@ -0,0 +1,33 @@
+package RandomQuestions;
+
+import java.util.*;
+
+public class A_1155_ReverseASubstring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String s = sc.nextLine();
+        int[] ans = solve(n, s);
+        if(ans[0] == -1){
+            System.out.println("NO");
+        }
+        else{
+            System.out.println("YES");
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+
+    public static int[] solve(int n, String s){
+        int i = 1;
+        while(i < n){
+            char c = s.charAt(i);
+            char p = s.charAt(i-1);
+            if(c < p){
+                return new int[]{i, i+1};
+            }
+            i++;
+        }
+        return new int[]{-1};
+    }
+}
---
+++ src/RandomQuestions/B_1312_BogoSort.java
@@ -0,0 +1,35 @@
+package RandomQuestions;
+
+import java.util.*;
+
+public class B_1312_BogoSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int[][] ans = new int[t][];
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int j = 0; j < n; j++) {
+                arr[j] = sc.nextInt();
+            }
+            int[] res = solve(n, arr);
+            ans[i] = res;
+        }
+        for (int i = 0; i < t; i++) {
+            for (int j = 0; j < ans[i].length; j++) {
+                System.out.print(ans[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static int[] solve(int n, int[] arr){
+        Arrays.sort(arr);
+        int[] ans = new int[n];
+        for (int i = n-1; i >= 0; i--) {
+            ans[i] = arr[n-i-1];
+        }
+        return ans;
+    }
+}
